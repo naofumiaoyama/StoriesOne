3a9f51859f92abaecad25ea9e60f1b3b59521da5
Toya Arai <arty108.11.23.intt@gmail.com>
2021-04-04 13:08:48 +0900

    no message

--- Stories.Api/Controllers/UserController.cs
+++ Stories.Api/Controllers/UserController.cs
@@ -34,5 +34,101 @@ namespace Stories.Api.Controllers
             users.Add(user);
             return users.ToArray();
         }
+
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<User>>>GetTodoItem()
+        //{
+        //    return await _context.TodoItems
+        //    .Select(x => ItemToDTO(x))
+        //    .ToListAsync();
+        //}
+
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<User>> GetTodoItem(long id)
+        //{
+        //    var todoItem = await _context.TodoItems.FindAsync(id);
+
+        //    if (UserAccount == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return ItemToDTO(todoItem);
+        //}
+
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> UpdateTodoItem(long id, TodoItemDTO todoItemDTO)
+        //{
+        //    if (id != todoItemDTO.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    var todoItem = await _context.TodoItems.FindAsync(id);
+        //    if (todoItem == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    todoItem.Name = todoItemDTO.Name;
+        //    todoItem.IsComplete = todoItemDTO.IsComplete;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return NoContent();
+        //}
+
+        //[HttpPost]
+        //public async Task<ActionResult<TodoItemDTO>> CreateTodoItem(TodoItemDTO todoItemDTO)
+        //{
+        //    var todoItem = new TodoItem
+        //    {
+        //        IsComplete = todoItemDTO.IsComplete,
+        //        Name = todoItemDTO.Name
+        //    };
+
+        //    _context.TodoItems.Add(todoItem);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction(
+        //        nameof(GetTodoItem),
+        //        new { id = todoItem.Id },
+        //        ItemToDTO(todoItem));
+        //}
+
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteTodoItem(long id)
+        //{
+        //    var todoItem = await _context.TodoItems.FindAsync(id);
+
+        //    if (todoItem == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.TodoItems.Remove(todoItem);
+        //    await _context.SaveChangesAsync();
+
+        //    return NoContent();
+        //}
+
+        //private bool TodoItemExists(long id) =>
+        //     _context.TodoItems.Any(e => e.Id == id);
+
+        //private static TodoItemDTO ItemToDTO(TodoItem todoItem) =>
+        //    new TodoItemDTO
+        //    {
+        //        Id = todoItem.Id,
+        //        Name = todoItem.Name,
+        //        IsComplete = todoItem.IsComplete
+        //    };
+
     }
 }
