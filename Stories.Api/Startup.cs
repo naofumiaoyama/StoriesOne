abd68ab000d0f94f8ab1fc6aa0cb48b1d8328142
Jenalyn Aoyama <albiosjenalyn27@gmail.com>
2021-05-22 12:28:57 +0900

    Adding Test Project

--- Stories.Api/Startup.cs
+++ Stories.Api/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Stories.Data;
 
 namespace Stories.Api
 {
@@ -26,6 +28,9 @@ namespace Stories.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("sqlServerConnection"))
+                );
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
