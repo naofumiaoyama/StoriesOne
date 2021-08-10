6b90c9f7e03339207ecc4bca4efd5c888dfd73c6
Naofumi Aoyama <naofumi.aoyama@gmail.com>
2021-08-10 14:47:15 +0900

    Changing Encrypt logic

--- Stories.Utility/EncryptDecrypt.cs
+++ Stories.Utility/EncryptDecrypt.cs
@@ -9,28 +9,31 @@ namespace Stories.Utility
 {
    public class EncryptDecrypt
     {
-        public static string hash = "St0r1es";
         public static string Encrypt(string value)
         {
-            byte[] data = UTF8Encoding.UTF8.GetBytes(value);
-            using (MD5CryptoServiceProvider mD5 = new MD5CryptoServiceProvider())
-            {
-                byte[] keys = mD5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
-                using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
-                {
-                    ICryptoTransform transform = tripDes.CreateEncryptor();
-                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
-                   return Convert.ToBase64String(results, 0, results.Length);
-                }
-            }
+            string toEncrypt = value;
+            string key = "St0r1es";
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+            byte[] keyArray = hashmd5.ComputeHash(UnicodeEncoding.Unicode.GetBytes(key));
+            tdes.Key = keyArray;
+            tdes.Mode = CipherMode.ECB;
+
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+            byte[] toEncryptArray = UnicodeEncoding.Unicode.GetBytes(toEncrypt);
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            string finalString = Convert.ToBase64String(resultArray);
+            return finalString;
         }
 
         public static string Decrypt(string value)
         {
+            string key = "St0r1es";
             byte[] data = Convert.FromBase64String(value);
             using (MD5CryptoServiceProvider mD5 = new MD5CryptoServiceProvider())
             {
-                byte[] keys = mD5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                byte[] keys = mD5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
                 using (TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
                 {
                     ICryptoTransform transform = tripDes.CreateDecryptor();
